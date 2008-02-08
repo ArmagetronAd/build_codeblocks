8b1551e65277fb36fa779938d84a0a8163a9e976
z-man <Unknown>
2008-02-08 18:21:54 +0000

    Copied over from build_visualc. We don't use Visual C any more, so this here is a better place for the working version.

--- makedist.bat
+++ makedist.bat
@@ -1,11 +1,13 @@
 @echo off
-REM This batch file can be used to build the target directories "dist" and "debug" two layers up from here where the release
-REM and debug binaries will be compiled to.
+REM This batch file can be used to build the target directories "dist" and "debug" one layer
+REM up from here where the release and debug binaries will be compiled to.
 
-SET AA_DIR=..\armagetronad
-SET LIBS_DIR=..\winlibs
-SET DIST_DIR=..\dist
-SET DEBUG_DIR=..\debug
+SET AA_DIR="..\armagetronad"
+SET LIBS_DIR="..\winlibs"
+SET DIST_DIR_BASE="dist"
+SET DEBUG_DIR_BASE="debug"
+SET DIST_DIR="..\%DIST_DIR_BASE%"
+SET DEBUG_DIR="..\%DEBUG_DIR_BASE%"
 
 echo making directory...
 REM del %DIST_DIR% /S /Q
@@ -27,7 +29,10 @@ xcopy %AA_DIR%\language %DIST_DIR%\language /I /E /Y
 xcopy %AA_DIR%\models %DIST_DIR%\models /I /E /Y
 xcopy %AA_DIR%\music %DIST_DIR%\music /I /E /Y
 xcopy %AA_DIR%\sound %DIST_DIR%\sound /I /E /Y
-xcopy %AA_DIR%\resource %DIST_DIR%\resource /I /E /Y
+md %DIST_DIR%\resource
+xcopy %AA_DIR%\resource\proto %DIST_DIR%\resource\included /I /E /Y
+xcopy %AA_DIR%\resource\included %DIST_DIR%\resource\included /I /E /Y
+xcopy %AA_DIR%\batch\make\sortresources.py %DIST_DIR%\resource\included /I /E /Y
 xcopy %AA_DIR%\textures %DIST_DIR%\textures /I /E /Y
 xcopy %AA_DIR%\*.txt %DIST_DIR% /I /Y
 copy %AA_DIR%\README %DIST_DIR%\README.txt /Y
@@ -40,8 +45,18 @@ xcopy %LIBS_DIR%\SDL_image\VisualC\graphics\lib\*.dll %DIST_DIR% /Y
 xcopy %LIBS_DIR%\libxml2\lib\*.dll %DIST_DIR% /Y
 xcopy %LIBS_DIR%\iconv\lib\*.dll %DIST_DIR% /Y
 
+echo copying installation files...
+xcopy *.nsi %DIST_DIR% /Y
+xcopy *.bmp %DIST_DIR% /Y
+xcopy *.url %DIST_DIR% /Y
+
+echo moving resources
+cd %DIST_DIR%\resource\included
+sortresources.py
+cd ..\..\..
+
 echo making debug...
-xcopy %DIST_DIR% %DEBUG_DIR% /I /E /Y
+xcopy %DIST_DIR_BASE% %DEBUG_DIR_BASE% /I /E /Y
 
 SET AA_DIR=
 SET LIBS_DIR=
